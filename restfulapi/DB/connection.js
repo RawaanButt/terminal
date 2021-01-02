9da15c129a0f903a75e8e4f59fe06c2830d74cd7
RawaanButt <71717689+RawaanButt@users.noreply.github.com>
2021-01-02 16:41:56 +0500

    api

---
+++ restfulapi/DB/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+var config = require("config");
+
+const connectDB = async () => {
+  await mongoose.connect(config.get("db"), {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  console.log("Connected to Db");
+};
+
+module.exports = connectDB;
